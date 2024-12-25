a45b9e4febcaa88c9e1b4927528f6f29e27bdab3
Greg <sogmeadmug@gmail.com>
2024-12-26 01:06:31 +0300

    функции для csv

--- files_utils.py
+++ files_utils.py
@@ -47,3 +47,56 @@ def append_json(data: list[dict], file_path: str, encoding: str = "utf-8")-> Non
 
     with open(file_path, "w", encoding= encoding) as file:
         json.dump(python_data, file, ensure_ascii= False, indent= 4)
+
+
+def read_csv(file_path, delimiter=';', encoding: str ='windows-1251')-> list:
+    """
+    Описание: Читает данные из CSV-файла.
+    Входные параметры:
+    file_path: Путь к файлу.
+    delimiter: Разделитель полей в файле (по умолчанию `';'`).
+    encoding: Кодировка файла (по умолчанию `"windows-1251"`).
+    Возвращаемое значение:
+    Данные, считанные из файла.
+    """
+    input_list = []
+    with open(file_path, "r", encoding= encoding) as file:
+        reader = csv.DictReader(file, delimiter= delimiter)
+        input_list = list(reader)
+    return input_list
+
+
+def write_csv(data: list, file_path, delimiter=';', encoding: str ='windows-1251')-> None:
+    """
+    Описание:
+    Записывает данные в CSV-файл.
+    Входные параметры:
+    data: Данные для записи.
+    file_path: Путь к файлу.
+    delimiter: Разделитель полей в файле (по умолчанию `';'`).
+    encoding: Кодировка файла (по умолчанию `"windows-1251"`).
+    Возвращаемое значение:
+    Нет.
+    """
+    with open(file_path, "w", encoding= encoding) as file:
+        writer = csv.DictWriter(
+        file, fieldnames=data[0].keys(), delimiter= delimiter, lineterminator="\n"
+        )
+        writer.writeheader()
+        writer.writerows(data)
+
+
+def append_csv(data, file_path, delimiter=';', encoding: str ='windows-1251')-> None:
+    """
+    Описание: Добавляет данные в существующий CSV-файл.
+    Входные параметры:
+    data: Данные для добавления.
+    file_path: Путь к файлу.
+    delimiter: Разделитель полей в файле (по умолчанию `';'`).
+    encoding: Кодировка файла (по умолчанию `"windows-1251"`).
+    Возвращаемое значение:
+    Нет.
+    """
+    with open(file_path, "a", encoding= encoding) as file:
+        writer = csv.writer(file, delimiter= delimiter, lineterminator="\n")
+        writer.writerow(data)
